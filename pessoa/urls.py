27f6b708d4c5b1d5aef5bd691a98eb28b95d0a12
Jr Bill <jrbill@hackermail.com>
2021-12-17 01:24:43 -0300

    Cadastro simples

---
+++ pessoa/urls.py
@@ -0,0 +1,9 @@
+from os import name
+from django.urls import path
+from .views import ListaPessoaView, PessoaCreateView
+
+
+urlpatterns = [
+    path('', ListaPessoaView.as_view(), name='pessoa.index'),
+    path('novo/', PessoaCreateView.as_view(), name='pessoa.novo')
+]
